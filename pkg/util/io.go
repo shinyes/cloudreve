325c977cef8a8aafc65dcfb4f4e9f6869e70d5ed
HFO4 <912394456@qq.com>
2019-12-13 12:51:23 +0800

    Feat: add go.sum

--- pkg/util/io.go
+++ pkg/util/io.go
@@ -1,9 +1,6 @@
 package util
 
-import (
-	"os"
-	"path/filepath"
-)
+import "os"
 
 // Exists reports whether the named file or directory exists.
 func Exists(name string) bool {
@@ -13,18 +10,4 @@ func Exists(name string) bool {
 		}
 	}
 	return true
-}
-
-// CreatNestedFile 给定path创建文件，如果目录不存在就递归创建
-func CreatNestedFile(path string) (*os.File, error) {
-	basePath := filepath.Dir(path)
-	if !Exists(basePath) {
-		err := os.MkdirAll(basePath, 0700)
-		if err != nil {
-			Log().Warning("无法创建目录，%s", err)
-			return nil, err
-		}
-	}
-
-	return os.Create(path)
 }

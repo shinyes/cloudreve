796639945e0cf53e0a4d2d689a8cf5a259fc85a9
HFO4 <912394456@qq.com>
2020-11-21 18:19:24 +0800

    Merge branch 'master' of https://github.com/cloudreve/Cloudreve

--- pkg/conf/conf.go
+++ pkg/conf/conf.go
@@ -122,15 +122,15 @@ func Init(path string) {
 	}
 
 	sections := map[string]interface{}{
-		"Database":  DatabaseConfig,
-		"System":    SystemConfig,
-		"SSL":       SSLConfig,
-		"Unix":      UnixConfig,
-		"Captcha":   CaptchaConfig,
-		"Redis":     RedisConfig,
-		"Thumbnail": ThumbConfig,
-		"CORS":      CORSConfig,
-		"Slave":     SlaveConfig,
+		"Database":   DatabaseConfig,
+		"System":     SystemConfig,
+		"SSL":        SSLConfig,
+		"UnixSocket": UnixConfig,
+		"Captcha":    CaptchaConfig,
+		"Redis":      RedisConfig,
+		"Thumbnail":  ThumbConfig,
+		"CORS":       CORSConfig,
+		"Slave":      SlaveConfig,
 	}
 	for sectionName, sectionStruct := range sections {
 		err = mapSection(sectionName, sectionStruct)
